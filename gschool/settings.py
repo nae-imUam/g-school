1fe8454a63a7d54495555d3cbbc036d8945df60c
Nae Imuam <53511556+nae-imUam@users.noreply.github.com>
2022-04-03 11:54:03 +0530

    update settings

--- gschool/settings.py
+++ gschool/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-czrs4ax#yj((7we)^mvgs5y8bv6u3*(f(1c2#%_yx7o(5c8=-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://gschool.herokuapp.com/']
 
